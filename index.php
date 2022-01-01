3816bfa8f45b6d60d76b742efbd20d9b75844f4f
Alexandr <kosianlucky@mail.ru>
2022-01-01 17:00:27 +0300

    start_lesson

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+    $nickname=" Alexandr";
+    $hello="Привет";
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP цикл foreach</title>
+</head>
+<body>
+    <header class="">
+        <h2 class="subtitle">Массив и цикл foreach.</h2>
+    </header>
+    <div class="step__first">
+        
+    </div>
+    
+</body>
+</html>
